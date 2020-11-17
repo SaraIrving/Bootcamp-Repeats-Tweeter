14754ad003a2790dc5b158b8ac017144e275a4b7
Sara Irving <sarakirving@gmail.com>
2020-11-17 02:22:39 +0000

    refactor to use .val instead of .html to set the dynamic counter value

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -19,6 +19,9 @@ $(document).ready(function() {
    //find the counter but going up the DOM to it's parent and then searching what it holds
    const counter = $(this).parent().find(".counter");
 
+   //use jquery to dynamically update the count displayed
+   $(counter).val(inputLength);
+
   //  //if it's longer than 140, turn the counter red
   //  //if it's less than or equal to 140 keep it grey
    inputLength > maxLength ? $(counter).css("color", "red") : $(counter).css("color", "#545149");
