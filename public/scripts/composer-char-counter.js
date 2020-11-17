be784491cedbad81ecdfaac76cc3f51b4db74e1f
Sara Irving <sarakirving@gmail.com>
2020-11-17 02:12:08 +0000

    determine the length of the tweet being composed and have the counter turn red if it goes over the character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,6 +6,23 @@
 $(document).ready(function() {
   // test this code is running after all html is loaded
   //console.log("DOM is all loaded up!");
+  
+  // track how many characters are being typed in the Textarea tag
+  //remember not to use arrow functions as they do not bind the context of this
 
+ $("#tweet-text").keyup(function() {
+   
+   const inputLength = $(this).val().length;
+
+   const maxLength = 140;
+
+   //find the counter but going up the DOM to it's parent and then searching what it holds
+   const counter = $(this).parent().find(".counter");
+
+  //  //if it's longer than 140, turn the counter red
+  //  //if it's less than or equal to 140 keep it grey
+   inputLength > maxLength ? $(counter).css("color", "red") : $(counter).css("color", "#545149");
+
+ });
 
 });
