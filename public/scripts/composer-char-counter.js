e4d1ed6698023f6cbd3c5ebbcbf73acf13370b19
Sara Irving <sarakirving@gmail.com>
2020-11-17 01:20:01 +0000

    create file for the js code that will run the character counter for the tweet form

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+//check it's loaded into the index.html
+//console.log("It loaded!")
+
+
+//$(document).ready runs a callback when the DOM is ready to be manipulated with jQuery.
+$(document).ready(function() {
+  // test this code is running after all html is loaded
+  //console.log("DOM is all loaded up!");
+
+
+});
