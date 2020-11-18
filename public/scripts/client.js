d6b3bb9413cbb0f5dfe9baa381348b935544f769
Sara Irving <sarakirving@gmail.com>
2020-11-18 19:18:51 +0000

    create a function formatDate which uses the moment library to fomat the timestamp provided into a fuzzy date to display on the tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
  $(document).ready( function() {
 
   const tweetData =    {
@@ -20,29 +21,35 @@
 
   // Fake data taken from initial-tweets.json
 const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const formatDate = function(timestamp) {
+    const dateCreated = moment(timestamp).fromNow();
+
+    return dateCreated;
+  };
 
   const createTweetElement = function(tweetObject) {
   
@@ -59,7 +66,7 @@ const data = [
                       <p>${tweetObject.content.text}</p>
                     </div>
                     <footer>
-                      <p>${tweetObject.created_at}</p>
+                      <p>${formatDate(tweetObject.created_at)}</p>
                       <div class="icons">
                         <i class="fa fa-flag" aria-hidden="true"></i>
                         <i class="fa fa-retweet" aria-hidden="true"></i>
@@ -85,14 +92,5 @@ const data = [
   };
 
   renderTweets(data);
-
-
-
-    // const $tweet = createTweetElement(tweetData)
-    // console.log($tweet);
-
-    // $(".tweet-container").append($tweet)
-
-
 
  });
