386c7647be675b95fb9d0846a8fea7bd9cbc596c
Sara Irving <sarakirving@gmail.com>
2020-11-18 18:33:51 +0000

    create render tweets funciton which will leverage the createTweetElement function to take in an array of tweet objects, crate the html for them and then append them to the tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,7 +16,33 @@
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
     "created_at": 1461116232227
- }
+  };
+
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
   const createTweetElement = function(tweetObject) {
   
@@ -45,9 +71,28 @@
     return $tweet
   };
 
-    const $tweet = createTweetElement(tweetData)
-    console.log($tweet);
 
-    $(".tweet-container").append($tweet)
+  const renderTweets = function(tweetArray) {
+
+    //loop through array and pull out each tweet object
+    for (let tweetObj of tweetArray) {
+      // create the html for that tweet
+      const $tweet = createTweetElement(tweetObj);
+
+      // append it to the tweet-container 
+      $(".tweet-container").append($tweet);
+    };
+  };
+
+  renderTweets(data);
+
+
+
+    // const $tweet = createTweetElement(tweetData)
+    // console.log($tweet);
+
+    // $(".tweet-container").append($tweet)
+
+
 
  });
